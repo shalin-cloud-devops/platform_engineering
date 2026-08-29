ffe6875c7396d1ffe7712100ed0a99f51da50107
shalin-cloud-devops <shalin.citi.devops@gmail.com>
2026-08-29 10:59:33 +0530

    placehoklders

--- cluster_iam_rbac/terraform.tfvars
+++ cluster_iam_rbac/terraform.tfvars
@@ -1,1 +1,2 @@
-aws_region = "us-east1"
+aws_region   = "us-east1"
+cluster_name = "mutual-fund-app-cluster"
--- cluster_iam_rbac/variables.tf
+++ cluster_iam_rbac/variables.tf
@@ -1,6 +1,9 @@
 variable "aws_region" {
   description = "Default AWS region"
   type        = string
-  default     = "us-east-1"
+}
 
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
 }
