97e15ce81c35b0ea33de381406f20cde9cfa4cf1
shalin-cloud-devops <shalin.citi.devops@gmail.com>
2026-08-29 14:44:32 +0530

    placehoklders

--- cluster_iam_rbac/access_entry.tf
+++ cluster_iam_rbac/access_entry.tf
@@ -1,4 +1,4 @@
-resource "aws_eks_access_entry" "example" {
+resource "aws_eks_access_entry" "eks_access_entry" {
   cluster_name      = var.cluster_name
   principal_arn     = aws_iam_role.fund_fetch_role.arn
   kubernetes_groups = ["fund-fetchers-svc"]
