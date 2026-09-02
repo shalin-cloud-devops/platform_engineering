4625ab7c0c886ac08046ae131a14bd8f4eab8bfb
shalin-cloud-devops <shalin.citi.devops@gmail.com>
2026-09-02 21:14:58 +0530

    karpenter role and policy

---
+++ eks/karpenter_iam.tf
@@ -0,0 +1,122 @@
+resource "aws_iam_role" "karpenter_controller_role" {
+  name = "karpenter-controller-role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : module.eks.oidc_provider_arn
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+
+            "${module.eks.oidc_provider}:sub" : "system:serviceaccount:kube-system:karpenter",
+            "${module.eks.oidc_provider}:aud" : "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+
+
+  tags = {
+    Service = "Karpenter Controller"
+    Team    = "Platform Engineering - Dev"
+  }
+
+}
+
+resource "aws_iam_policy" "karpenter_controller_policy" {
+  name = "karpenter-controller-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid      = "AllowScopedEC2InstanceActions"
+        Effect   = "Allow"
+        Resource = "*"
+        Action = [
+          "ec2:RunInstances",
+          "ec2:CreateFleet",
+          "ec2:CreateLaunchTemplate",
+          "ec2:CreateTags",
+          "ec2:TerminateInstances",
+          "ec2:DeleteLaunchTemplate"
+        ]
+      },
+      {
+        Sid      = "AllowRegionalReadActions"
+        Effect   = "Allow"
+        Resource = "*"
+        Action = [
+          "ec2:DescribeInstances",
+          "ec2:DescribeLaunchTemplates",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeImages",
+          "ec2:DescribeInstanceTypes",
+          "ec2:DescribeInstanceTypeOfferings",
+          "ec2:DescribeAvailabilityZones",
+          "ec2:DescribeSpotPriceHistory"
+        ]
+      },
+      {
+        Sid      = "AllowSSMReadActions"
+        Effect   = "Allow"
+        Resource = "*"
+        Action   = "ssm:GetParameter"
+      },
+      {
+        Sid      = "AllowPricingReadActions"
+        Effect   = "Allow"
+        Resource = "*"
+        Action   = "pricing:GetProducts"
+      },
+      {
+        Sid      = "AllowInterruptionQueueActions"
+        Effect   = "Allow"
+        Resource = aws_sqs_queue.karpenter_interruption_queue.arn
+        Action = [
+          "sqs:DeleteMessage",
+          "sqs:GetQueueUrl",
+          "sqs:ReceiveMessage"
+        ]
+      },
+      {
+        Sid      = "AllowPassingInstanceRole"
+        Effect   = "Allow"
+        Resource = module.eks.eks_managed_node_groups["mutual_fund_nodes"].iam_role_arn
+
+        Action = "iam:PassRole"
+      },
+      {
+        Sid      = "AllowScopedInstanceProfileActions"
+        Effect   = "Allow"
+        Resource = "*"
+        Action = [
+          "iam:CreateInstanceProfile",
+          "iam:TagInstanceProfile",
+          "iam:AddRoleToInstanceProfile",
+          "iam:RemoveRoleFromInstanceProfile",
+          "iam:DeleteInstanceProfile",
+          "iam:GetInstanceProfile"
+        ]
+      },
+      {
+        Sid      = "AllowEKSDescribe"
+        Effect   = "Allow"
+        Resource = "*"
+        Action   = "eks:DescribeCluster"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "karpenter_controller_attach" {
+  role       = aws_iam_role.karpenter_controller_role.name
+  policy_arn = aws_iam_policy.karpenter_controller_policy.arn
+}
